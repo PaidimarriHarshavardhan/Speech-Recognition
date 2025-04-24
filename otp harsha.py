599edcf8c7101afc81d02f2b079a965cd5312477
PaidimarriHarshavardhan <paidiamarrih@gmail.com>
2025-04-24 15:15:02 +0530

    Add files via upload

---
+++ otp harsha.py
@@ -0,0 +1,53 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import random
+
+def otp(to_email):
+    otp = random.randint(1111,9999)
+
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+
+    username = "paidiamarrih@gmail.com"
+    password = "xdnj ltws rowu ajwp"
+
+    from_email = "paidiamarrih@gmail.com"
+    subject = "OTP for Verification"
+    body = f"The OPT for Verirfication is {otp}"
+
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body,"plain"))
+
+    server = smtplib.SMTP(smtp_server,smtp_port)
+    server.starttls()
+    server.login(username,password)
+    server.send_message(msg)
+    server.quit()
+    print("Email Sent !")
+
+    votp = int(input("Enter Otp: "))
+    if votp == otp:
+        print("Verification Success")
+    else:
+        print("Verirification Failed")
+otp("ashoksudireddy15@gmail.com")
+otp("paidiamarrih@gmail.com")
+otp("nithishkumarbollapelli@gmail.com")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
